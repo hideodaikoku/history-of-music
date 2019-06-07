8186b231ea2b16b6766524738f62629c3efb4740
hideo <neildaikoku@gmail.com>
2019-06-07 12:18:35 +0900

    updated data strucures

---
+++ data.js
@@ -0,0 +1,13 @@
+const data  = [
+    {
+        id: "ID NUMBER",
+        country : "COUNTRY NAME",
+        lat : "LATITUDE",
+        long: "LONGITUDE",
+        year: "YEAR",
+        audio: "AUDIO FILE NAME",
+        page: "PAGE NUMBER",
+        genre: "GATEWAY IT REFERS TO",
+        tags: ["ADDITIIONAL","TAGS","STORED","IN","AN","ARRAY"]
+    }
+]
