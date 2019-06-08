28f6ad4f272bba6bb966f5ce7f46ca967ba6d83d
hideo <neildaikoku@gmail.com>
2019-06-09 02:34:16 +0900

    updated data from text

--- data.js
+++ data.js
@@ -1,13 +1,123 @@
+// TODO: MAKE A DATABASE FOR SONGS
 const data  = [
     {
-        id: "ID NUMBER",
-        country : "COUNTRY NAME",
-        lat : "LATITUDE",
-        long: "LONGITUDE",
-        year: "YEAR",
-        audio: "AUDIO FILE NAME",
-        page: "PAGE NUMBER",
-        genre: "GATEWAY IT REFERS TO",
-        tags: ["ADDITIIONAL","TAGS","STORED","IN","AN","ARRAY"]
+        id: "1",
+        country : "CONGO",
+        lat : "-3.833562",
+        long: "24.339626",
+        year: -2000,
+        culture: "BaAka",
+        audio: "01 Mabo_Bisengo Bwa Bolé_.mp3",
+        page: "22",
+        genre: "MUSIC OF FORAGERS",
+        tags: ["SMALL SCALE SOCIETIES"]
+    },
+    {
+        id: "2",
+        country : "TUVA",
+        lat : "51.736420",
+        long: "94.031990",
+        year: -1000,
+        culture: "Tuva",
+        audio: "02 Borbangnadyr with Steam Water.mp3",
+        page: "28",
+        genre: "MUSIC OF NOMADIC PASTORALISTS",
+        tags: ["SMALL SCALE SOCIETIES"]
+    },
+    {
+        id: "3",
+        country : "MALAITA",
+        lat : "-8.924688",
+        long: "160.981026",
+        year: -2000,
+        culture: "Are'are",
+        audio: "03 Pisi ni tootora.mp3",
+        page: "33",
+        genre: "MUSIC OF HORTICULTURAL SOCIETIES",
+        tags: ["SMALL SCALE SOCIETIES"]
+    },
+    {
+        id: "4",
+        country : "TIBET",
+        lat : "30.153360",
+        long: "88.787865",
+        year: -100,
+        culture: "Tibetan Buddhists",
+        audio: "04 Invoking The Spirit Of KindnessThrough Sound.mp3",
+        page: "40",
+        genre: "BUDDHIST MUSIC",
+        tags: ["ANCIENT AND MEDIEVAL RELGIOUS MUSIC"]
+    },
+    {
+        id: "5",
+        country : "GERMANY",
+        lat : "45.714809",
+        long: "-121.466469",
+        year: 1098,
+        culture: "Roman Catholics",
+        audio: "05 Quia ergo femina.mp3",
+        page: "46",
+        genre: "CHRISTIAN CHANT",
+        tags: ["ANCIENT AND MEDIEVAL RELGIOUS MUSIC"]
+    },
+    {
+        id: "6",
+        country : "SYRIA",
+        lat : "33.513805",
+        long: "36.276527",
+        year: 750,
+        culture: "Islam",
+        audio: "06 Chanting (tartîl) Of the Qur_ân.mp3",
+        page: "54",
+        genre: "QUR'ANIC CHANT",
+        tags: ["ANCIENT AND MEDIEVAL RELGIOUS MUSIC"]
+    },
+    {
+        id: "7",
+        country : "FRANCE",
+        lat : "49.256599",
+        long: "4.033090",
+        year: 1300,
+        culture: "Medieval Christian Church",
+        audio: "07 Messe de Nostre Dame_ Gloria.mp3",
+        page: "59",
+        genre: "EARLY EUROPEAN POLYPHONIC MUSIC",
+        tags: ["ANCIENT AND MEDIEVAL RELGIOUS MUSIC"]
+    },
+    {
+        id: "8",
+        country : "CHINA",
+        lat : "37.864447",
+        long: "86.258552",
+        year: 1491,
+        culture: "Medieval China",
+        audio: "08 Three Variations on the Yang Pass.mp3",
+        page: "68",
+        genre: "MUSIC OF CHINA",
+        tags: ["ANCIENT AND MEDIEVAL SECULAR MUSIC"]
+    },
+    {
+        id: "9",
+        country : "TURKEY",
+        lat : "41.008240",
+        long: "28.978359",
+        year: 1453,
+        culture: "Middle East",
+        audio: "09 Taqasim _ Sama_i Bayyati Al-Arayan.mp3",
+        page: "73",
+        genre: "MUSIC OF THE MIDDLE EAST",
+        tags: ["ANCIENT AND MEDIEVAL SECULAR MUSIC"]
+    },
+    {
+        id: "10",
+        country : "ZIMBABWE",
+        lat : "-17.827629",
+        long: "31.050449",
+        year: 1589,
+        culture: "Shona",
+        audio: "10 Nyamaropa.m4a",
+        page: "80",
+        genre: "AFRICAN MUSIC",
+        tags: ["ANCIENT AND MEDIEVAL SECULAR MUSIC"]
     }
 ]
